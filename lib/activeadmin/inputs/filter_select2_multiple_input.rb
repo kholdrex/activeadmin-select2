065e7e9268782bde9cea8559945ef729cfe34b3c
kholdrex <alexandrkholodniak@gmail.com>
2015-10-18 21:21:31 +0300

    ActiveAdmin 1.0.0.pre2 Support

--- lib/activeadmin/inputs/filter_select2_multiple_input.rb
+++ lib/activeadmin/inputs/filter_select2_multiple_input.rb
@@ -2,7 +2,7 @@ module ActiveAdmin
   module Inputs
     class FilterSelect2MultipleInput < Formtastic::Inputs::SelectInput
 
-      include ::ActiveAdmin::Inputs::FilterBase
+      include ::ActiveAdmin::Inputs::Filters::Base
 
       def input_name
         "#{super}_in"
